f680f0176417961d5a3b69617d8b6bc464f88f13
yuqiang <1054572107@qq.com>
2020-10-29 21:40:19 +0800

    Initial Commit

---
+++ Apple-SwiftUI-Example/Apple_SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_SwiftUI_ExampleApp.swift
+//  Apple-SwiftUI-Example
+//
+//  Created by qiang on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct Apple_SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-SwiftUI-ExampleTests/Apple_SwiftUI_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Apple_SwiftUI_ExampleTests.swift
+//  Apple-SwiftUI-ExampleTests
+//
+//  Created by qiang on 2020/10/29.
+//
+
+import XCTest
+@testable import Apple_SwiftUI_Example
+
+class Apple_SwiftUI_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
