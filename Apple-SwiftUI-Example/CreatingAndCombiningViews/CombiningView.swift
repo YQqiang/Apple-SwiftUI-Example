9b3172dc6f2c86ba514455e1d5ff5aa173a42e14
yuqiang <1054572107@qq.com>
2020-11-01 12:51:21 +0800

    调整 content view

---
+++ Apple-SwiftUI-Example/CreatingAndCombiningViews/CombiningView.swift
@@ -0,0 +1,40 @@
+//
+//  CombiningView.swift
+//  Apple-SwiftUI-Example
+//
+//  Created by yuqiang on 2020/11/1.
+//
+
+import SwiftUI
+
+struct CombiningView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Jushua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct CombiningView_Previews: PreviewProvider {
+    static var previews: some View {
+        CombiningView()
+    }
+}
