042fac0398efbc44c05e9098b3be7453a0a654db
yuqiang <1054572107@qq.com>
2020-11-03 21:49:05 +0800

    zStack

---
+++ Apple-SwiftUI-Example/DrawingPathsAndShapes/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Apple-SwiftUI-Example
+//
+//  Created by qiang on 2020/11/3.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .center)
+            
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
