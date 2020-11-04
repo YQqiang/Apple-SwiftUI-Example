0b009fd47c29db3284faf013fba5b5c1e3d4c8da
yuqiang <1054572107@qq.com>
2020-11-04 22:19:20 +0800

    AnimatingViewsAndTransitions

---
+++ Apple-SwiftUI-Example/AnimatingViewsAndTransitions/HikeView.swift
@@ -0,0 +1,20 @@
+//
+//  HikeView.swift
+//  Apple-SwiftUI-Example
+//
+//  Created by qiang on 2020/11/4.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
